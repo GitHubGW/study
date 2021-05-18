1cd8460fd01048e56167f04419816f5f2a0f8e0a
GW <kowonp@gmail.com>
2021-05-18 18:28:56 +0900

    To Do List

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,37 +1,70 @@
-import { createStore } from "redux";
+const { createStore } = require("redux");
 
-const number = document.getElementById("number");
-const addBtn = document.getElementById("addBtn");
-const minusBtn = document.getElementById("minusBtn");
+const form = document.querySelector('form');
+const todoInput = document.getElementById('todoInput');
+const todoSubmit = document.getElementById('todoSubmit');
+const ul = document.querySelector('ul');
 
-number.innerHTML = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case "handleAddBtn":
-      return count + 1;
-    case "handleMinusBtn":
-      return count - 1;
+
+const todoModifier = (state=[], action) => {
+  // console.log(state);
+  // console.log(action);
+
+  switch (action.type){
+    case ADD_TODO:
+      const newToDoObj = {text: action.text, id: Date.now()}
+      return [newToDoObj, ...state];
+    case DELETE_TODO:
+      return state.filter((stateObj)=>{
+        return stateObj.id !== parseInt(action.id);
+      });
     default:
-      return count;
+      return state;
   }
-};
+  
+}
 
-const countStore = createStore(countModifier);
+const todoStore = createStore(todoModifier);
 
-const onChange = () => {
-  number.innerHTML = countStore.getState();
-};
+const handleDeleteToDo = (event) => {
+  // console.log(event.target.parentNode.id);
+  todoStore.dispatch({type: DELETE_TODO, id: event.target.parentNode.id});
+  // console.log(todoStore.getState());
+}
 
-const handleAddBtn = () => {
-  countStore.dispatch({ type: "handleAddBtn" });
-};
+const handlePaintToDo = () => {
+  const toDoArray=todoStore.getState();
+  // console.log(toDoArray);
+  ul.innerHTML="";
+  
+  toDoArray.forEach((toDo)=>{
+    // console.log(toDo);
 
-const handleMinusBtn = () => {
-  countStore.dispatch({ type: "handleMinusBtn" });
-};
+    const li = document.createElement('li');
+    const button = document.createElement('button');
 
-countStore.subscribe(onChange);
+    li.id=toDo.id;
+    li.innerHTML=`<i class="fas fa-chevron-circle-right"></i> ${toDo.text}`;
 
-addBtn.addEventListener("click", handleAddBtn);
-minusBtn.addEventListener("click", handleMinusBtn);
+    button.innerHTML="❌";
+    button.addEventListener('click',handleDeleteToDo);
+
+    li.appendChild(button);
+    ul.appendChild(li);
+  })
+
+}
+
+const handleToDoInput = (event)=>{
+  event.preventDefault();
+  const todoInputValue = todoInput.value;
+  todoInput.value="";
+  todoStore.dispatch({type: ADD_TODO, text: todoInputValue})
+}
+
+todoStore.subscribe(handlePaintToDo);
+
+todoSubmit.addEventListener('click',handleToDoInput);
