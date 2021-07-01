b55c70703f8bdcf94dbfa3475db8ba71c5cfe74e
GW <kowonp@gmail.com>
2021-07-01 17:17:37 +0900

    React redux

---
+++ react-redux/src/store.js
@@ -0,0 +1,16 @@
+import {createStore} from redux;
+
+const ADD="ADD";
+const DELETE="DELETE";
+
+const addToDo=()=>{
+
+}
+
+const deleteToDo=()=>{
+
+}
+
+const store=createStore(reducer);
+
+export default store;
