e2f454ff3d46e734388135a0505dd812471e943e
GW <kowonp@gmail.com>
2021-07-19 00:53:55 +0900

    Detail Container

--- netflix-clone/src/Routes/Detail/DetailContainer.js
+++ netflix-clone/src/Routes/Detail/DetailContainer.js
@@ -1,15 +1,71 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { MovieApi, TVApi } from "api";
 
 class DetailContainer extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+      location: { pathname },
+    } = this.props;
+    // console.log(this.props);
+
+    const parsedId = parseInt(id);
+
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+
+    this.isMovie = pathname.includes("/movie/");
+    this.isTV = pathname.includes("/tv/");
+
+    try {
+      if (this.isMovie) {
+        const { data } = await MovieApi.movieDetail(parsedId);
+
+        this.setState({
+          result: data,
+        });
+      }
+
+      if (this.isTV) {
+        const { data } = await TVApi.tvDetail(parsedId);
+
+        this.setState({
+          result: data,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+
+      this.setState({
+        error: "DetailContainer Error",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
 
   render() {
+    // console.log(this.props);
+
     const { result, error, loading } = this.state;
+    console.log(this.state);
 
     return <DetailPresenter result={result} error={error} loading={loading}></DetailPresenter>;
   }
