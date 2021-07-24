08f8eebe8ac4a0c77732c7cfb3e4c2c843548de4
GW <kowonp@gmail.com>
2021-07-25 02:30:18 +0900

    Home, Section Presenter

--- netflix-clone/src/Components/Section.js
+++ netflix-clone/src/Components/Section.js
@@ -1,11 +1,18 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  margin-bottom: 50px;
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+  font-size: 22px;
+  font-weight: bold;
+`;
 
-const Children = styled.div``;
+const Children = styled.div`
+  display: block;
+`;
 
 const Section = ({ title, children }) => {
   return (
--- netflix-clone/src/Routes/Home/HomePresenter.js
+++ netflix-clone/src/Routes/Home/HomePresenter.js
@@ -7,7 +7,9 @@ const Container = styled.div``;
 const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) => {
   return loading ? null : (
     <Container>
-      <Section></Section>
+      {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing Movies">{nowPlaying.map((movie) => movie.title)}</Section>}
+      {upcoming && upcoming.length > 0 && <Section title="Upcoming Movies">{upcoming.map((movie) => movie.title)}</Section>}
+      {popular && popular.length > 0 && <Section title="Popular Movies">{popular.map((movie) => movie.title)}</Section>}
     </Container>
   );
 };
