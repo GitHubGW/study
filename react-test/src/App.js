e9e9f6efd869043798f39594cb1c43cefda55f57
GW <kowonp@gmail.com>
2021-09-04 17:28:43 +0900

    Component Life Cycle

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,45 +1,23 @@
-import Food from "./Food";
+/* eslint-disable react/no-direct-mutation-state */
+import React from "react";
 
-const foodArray = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image: "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 1.2,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image: "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.5,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image: "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 3,
-  },
-];
+class App extends React.Component {
+  state = {
+    movies: [],
+    loading: true,
+  };
 
-const App = () => {
-  return (
-    <>
-      {foodArray.map((food, index) => (
-        <Food key={food.id} foodName={food.name} foodImage={food.image} foodRating={food.rating}></Food>
-      ))}
-    </>
-  );
-};
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 2000);
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return <div>{loading ? "Loading.. .👑" : "Ready"}</div>;
+  }
+}
 
 export default App;
