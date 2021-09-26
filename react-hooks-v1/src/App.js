8c460220f339b786c580f969107db2bf5468ef19
GW <kowonp@gmail.com>
2021-09-26 17:48:13 +0900

    Initial commit

---
+++ react-hooks-v1/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import UseInput from "./UseInput";
+import UseTabs from "./UseTabs";
+
+const App = () => {
+  return (
+    <>
+      <UseInput></UseInput>
+      <UseTabs></UseTabs>
+    </>
+  );
+};
+
+export default App;
