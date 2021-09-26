d61f65c89a088e095ab7e88e8363972e08c495d0
GW <kowonp@gmail.com>
2021-09-26 17:47:52 +0900

    UseInput hook

---
+++ react-hooks-v1/src/UseInput.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const UseInput = () => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    if (value.length < 10 && !value.includes("@")) {
+      setValue(value);
+    }
+  };
+
+  return (
+    <>
+      <h1>UseInput</h1>
+      <form>
+        <input type="text" placeholder="Write your Name" value={value} onChange={onChange}></input>
+      </form>
+    </>
+  );
+};
+
+export default UseInput;
