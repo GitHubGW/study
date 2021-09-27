ac799030619ac69623b1ea5c9a8e53a3d204ac9f
GW <kowonp@gmail.com>
2021-09-27 19:10:52 +0900

    Test modal

--- react-test/react-animation-test/src/App.js
+++ react-test/react-animation-test/src/App.js
@@ -1,10 +1,34 @@
-import Router from "Router";
+import React, { useState } from "react";
+import Modal from "react-modal";
+// import Router from "Router";
+
+Modal.setAppElement("#root");
 
 function App() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+    console.log("1a");
+  }
+
+  function afterOpenModal() {
+    console.log("2a");
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+    console.log("3a");
+  }
   return (
     <>
       <h1>App</h1>
-      <Router />
+      {/* <Router /> */}
+      <button onClick={openModal}>Open Modal</button>
+      <Modal isOpen={modalIsOpen} onAfterOpen={afterOpenModal} onRequestClose={closeModal} contentLabel="Example Modal">
+        <h1>Hello</h1>
+        <button onClick={closeModal}>close</button>
+      </Modal>
     </>
   );
 }
