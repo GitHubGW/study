2a00e6c95e69b0006d440bd2e1d50db3e6795fc4
GW <kowonp@gmail.com>
2021-10-03 17:03:59 +0900

    Update hook

--- react-hooks-v1/src/App.js
+++ react-hooks-v1/src/App.js
@@ -8,10 +8,12 @@ import UsePreventLeave from "./UsePreventLeave";
 import UseBeforeLeave from "./UseBeforeLeave";
 import UseFadeIn from "./UseFadeIn";
 import UseNetwork from "./UseNetwork";
+import UseScroll from "./UseScroll";
+import UseFullScreen from "./UseFullScreen";
 
 const App = () => {
   return (
-    <>
+    <div style={{ width: "200vh", height: "300vh" }}>
       <UseInput />
       <UseTabs />
       <UseTitle />
@@ -21,7 +23,9 @@ const App = () => {
       <UseBeforeLeave />
       <UseFadeIn />
       <UseNetwork />
-    </>
+      <UseScroll />
+      <UseFullScreen />
+    </div>
   );
 };
 
