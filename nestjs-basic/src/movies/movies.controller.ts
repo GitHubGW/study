ab1d903ffbd4c68c910f74b1c0b6a974eafdb6e3
GW <kowonp@gmail.com>
2022-02-08 00:11:03 +0900

    Movies Controller

--- nestjs-basic/src/movies/movies.controller.ts
+++ nestjs-basic/src/movies/movies.controller.ts
@@ -8,36 +8,38 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dtos/createMovie.dto';
+import { MovieEntity } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('/movies')
 export class MoviesController {
-  @Get('/')
-  getAllMovies() {
-    return 'getAllMovies';
+  constructor(private readonly moviesService: MoviesService) {
+    console.log('moviesService', moviesService);
   }
 
-  @Get('/search')
-  searchMovie(@Query('year') year: string) {
-    return `searchMovie by year: ${year}`;
+  @Get('/')
+  getAllMovies(): MovieEntity[] {
+    return this.moviesService.getAllMovies();
   }
 
   @Get('/:id')
-  getMovie(@Param('id') id: string) {
-    return `getMovie ${id}`;
+  getMovie(@Param('id') id: number): MovieEntity {
+    return this.moviesService.getMovie(id);
   }
 
   @Post('/')
-  createMovie(@Body() data: object) {
-    return data;
+  createMovie(@Body() createMovieDto: CreateMovieDto): boolean {
+    return this.moviesService.createMovie(createMovieDto);
   }
 
   @Delete('/:id')
-  deleteMovie(@Param('id') id: string) {
-    return `deleteMovie ${id}`;
+  deleteMovie(@Param('id') id: number): boolean {
+    return this.moviesService.deleteMovie(id);
   }
 
   @Patch('/:id')
-  updateMovie(@Param('id') id: string, @Body() data) {
-    return { updatedMovieId: id, ...data };
+  updateMovie(@Param('id') id: number, @Body() data: any): MovieEntity {
+    return this.moviesService.updateMovie(id, data);
   }
 }
